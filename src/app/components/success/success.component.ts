947f7c17160922a4badc2ef356fca6994469b8ef
Rodolfo Dias <rmdias29@gmail.com>
2018-02-17 18:47:14 +0100

    feat(Success): create component

---
+++ src/app/components/success/success.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './success.component.html',
+  styleUrls: ['./success.component.css']
+})
+export class SuccessComponent {
+  title = 'app';
+}
