6a05871abab524b19a2ace1faaa4860073375630
Rodolfo Dias <rmdias29@gmail.com>
2018-02-17 18:46:40 +0100

    feat(Schedule): create component

---
+++ src/app/components/schedule/schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { personalisationProgress } from '../order-progress-bar/personalisation-progress.interface';
+
+@Component({
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.css']
+})
+export class ScheduleComponent {
+  personalisationProgress:personalisationProgress = {
+    style: 'progress-100',
+    profile: 'progress-100',
+    box: 'progress-25'
+  }
+}
