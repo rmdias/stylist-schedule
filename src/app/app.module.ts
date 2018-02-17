c59693312084659d0dfed9981dbf615b3935d122
Rodolfo Dias <rmdias29@gmail.com>
2018-02-17 15:14:04 +0100

    feat(app): defining routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+// Routed Components
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { SuccessComponent } from './components/success/success.component';
+
+// Components
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/landing-page',  pathMatch: 'full' },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'success', component: SuccessComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ScheduleComponent,
+    SuccessComponent,
+
+    TopBarComponent    
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
